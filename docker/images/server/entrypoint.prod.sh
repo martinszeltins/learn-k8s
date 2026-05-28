c1094a2ff2331bc116527d7bb8055a7f9e88e577
Your Name <you@example.com>
2026-05-28 20:58:46 +0300

    Initial commit

--- docker/images/server/entrypoint.prod.sh
+++ docker/images/server/entrypoint.prod.sh
@@ -3,18 +3,6 @@ set -e
 
 cd /app
 
-# Copy .env from example if it doesn't exist
-if [ ! -f .env ]; then
-    cp .env.example .env
-fi
-
-# Generate APP_KEY if not set
-if ! grep -q '^APP_KEY=.\+' .env 2>/dev/null; then
-    php artisan key:generate
-fi
-
-# Start PHP-FPM in background
 php-fpm &
 
-# Start nginx in foreground
 exec nginx -g 'daemon off;'
